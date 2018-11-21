1004b9c8f8d218194adc6cafd9b43bd0aa872274
AHSEN <AHSEN@192.168.0.3>
2018-11-21 12:29:08 -0600

    JavaProject

---
+++ 1-My First Program/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");       //equals to console.log()
+		System.out.print("What is up?");          //ln makes you go next line
+		System.out.println("Im good man!");
+		
+
+	}
+
+}
